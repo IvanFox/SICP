203d2f3fd6001559ffc96ab415cd1b6e387b7f4c
IvanFox <zvezdo4eet@gmail.com>
2016-03-28 20:04:05 +0100

    pascal triangle and function 'f'

---
+++ chapter_1/pascal_triangle.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(define (pascal-triangle row col)
+  (cond ((> col row) 0)
+        ((or (= row 2) (= row 1) (= col 1) (= col row)) 1)
+        (else (+ (pascal-triangle (- row 1) (- col 1)) (pascal-triangle (- row 1) col)))))
+
+(define (f n)
+  (cond ((> 3 n) n)
+        (else (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3)))))))
+
+
+             
