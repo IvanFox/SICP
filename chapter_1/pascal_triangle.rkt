62f3e561610a0da9b73d48b3a038f08a70706d61
IvanFox <zvezdo4eet@gmail.com>
2016-03-28 20:25:41 +0100

    pascal-triangle foo improved

--- chapter_1/pascal_triangle.rkt
+++ chapter_1/pascal_triangle.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (define (pascal-triangle row col)
   (cond ((> col row) 0)
-        ((or (= row 2) (= row 1) (= col 1) (= col row)) 1)
+        ((or (= row 1) (= col 1) (= col row)) 1)
         (else (+ (pascal-triangle (- row 1) (- col 1)) (pascal-triangle (- row 1) col)))))
 
 (define (f n)
